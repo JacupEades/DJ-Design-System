48f26a6e6d0a199b4e90f8001a8bc2b5f90600e6
Jacob_Eades <jwae98@gmail.com>
2023-06-12 17:53:47 -0400

    Third Commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Link from "next/link";
 import styles from "@/styles/scss/Home.module.scss";
 
 export default function Home() {
@@ -9,8 +8,8 @@ export default function Home() {
 				<title>DJ Design System</title>
 				<meta name="description" content="Look at all these tokens!" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<Link rel="icon" href="/icon-192x192.png" />
-				<Link rel="manifest" href="/manifest.json" />
+				<link rel="icon" href="/icon-192x192.png" />
+				<link rel="manifest" href="/manifest.json" />
 			</Head>
 			<main className={styles.homeMain}>
 				<h1>Home Page</h1>
